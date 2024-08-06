3755f8d86a8cb46102c86ce1d059f286d066ef4e
milo-hurtamon <hurtado.camilo@gmail.com>
2024-08-06 15:15:01 -0500

    Adding core package

---
+++ ToDoExample/src/Core/pgsqlCRUD.java
@@ -0,0 +1,59 @@
+package Core;
+
+import java.sql.*;
+
+/*
+TODO:
+  Create the interfaces to separate the whole CRUD into detailed functionalities as SOLID propose
+ */
+
+public class pgsqlCRUD {
+    // Variable creation
+    private Statement pgsqlStatement;
+    private ResultSet pgsqlFetchResult;
+    private String sqlStmnt;
+    private String queryToFetch;
+
+    // Class constructor
+    public pgsqlCRUD() {
+        pgsqlStatement = null;
+        sqlStmnt = null;
+        pgsqlFetchResult = null;
+        queryToFetch = null;
+    }
+
+    // Function that allow us to insert any new register
+    public void insertToDoList(Connection conn, String description, String date, Boolean completed, String observation) throws SQLException {
+        this.pgsqlStatement = conn.createStatement();
+        this.sqlStmnt = "INSERT INTO todolist (description, todo_date, completed, observation) VALUES('"+description+"','"+date+"',"+completed+",'"+observation+"')";
+        this.pgsqlStatement.executeUpdate(this.sqlStmnt);
+        this.pgsqlStatement.close();
+    }
+
+    // Function that return all the records in the table
+    public ResultSet fetchToDoList (Connection conn, String typeOfFetch) throws SQLException {
+        this.pgsqlStatement = conn.createStatement();
+
+        // Analize what kind of query we are looking forward
+        if (typeOfFetch == "A") {
+            this.queryToFetch = "SELECT * FROM todolist;";
+        } else if (typeOfFetch == "C") {
+            this.queryToFetch = "SELECT * FROM todolist WHERE completed != false;";
+        } else {
+            this.queryToFetch = "SELECT * FROM todolist WHERE completed = false;";
+        }
+
+        this.pgsqlFetchResult = this.pgsqlStatement.executeQuery(this.queryToFetch);
+        return this.pgsqlFetchResult;
+    }
+
+    // Function that will update an existing record
+    public Integer updateToDoList (Connection conn, Integer todoID, String description, String date, Boolean completed, String observation) throws SQLException {
+        // Something to do here
+        this.pgsqlStatement = conn.createStatement();
+        this.sqlStmnt = "UPDATE todolist SET description = '"+description+"', todo_date = '"+date+"', completed = "+completed+", observation = '"+observation+"' WHERE todoid = "+todoID+";";
+        this.pgsqlStatement.executeUpdate(this.sqlStmnt);
+        this.pgsqlStatement.close();
+        return 200;
+    }
+}
---
+++ ToDoExample/src/pgsqlTodoMain.java
@@ -0,0 +1,54 @@
+import Core.pgsqlCRUD;
+import Core.pgsqlConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class pgsqlTodoMain {
+    // Trying to resolve the DB Connection
+    public static String jdbcURL = "jdbc:postgresql://localhost:5432/todo_example";
+    public static String username = "pgsql_testrole";
+    public static String password = "testingpsswrd";
+
+    public static Connection connection = null;
+
+    public static ResultSet queryResult = null;
+
+    // Calling connection object
+    public static pgsqlConnection pgsqlConn = new pgsqlConnection();
+    // Calling the CRUD object
+    public static pgsqlCRUD pgsqlCD = new pgsqlCRUD();
+
+    public static void main(String[] args) {
+        //Using the internal functions
+        connection = pgsqlConn.getPgsqlConnection(jdbcURL, username, password);
+
+        try {
+            // Insertion function
+            /*
+            pgsqlCD.insertToDoList(connection, "This is the first test", "2024-08-01", false, "");
+            pgsqlCD.insertToDoList(connection, "This is the second test", "2024-08-01", true, "");
+            pgsqlCD.insertToDoList(connection, "This is the third test", "2024-08-01", false, "");
+            pgsqlCD.insertToDoList(connection, "This is the four test", "2024-08-01", true, "");
+             */
+
+            // Fetching all the results
+            queryResult = pgsqlCD.fetchToDoList(connection, "T");
+            while (queryResult.next()) {
+                System.out.println("ToDo Description: "+queryResult.getString("description"));
+                System.out.println("ToDo Date: "+queryResult.getDate("todo_date"));
+                System.out.println("ToDo Completed: "+queryResult.getBoolean("completed"));
+                System.out.println("ToDo Observation: "+queryResult.getString("observation"));
+                System.out.println();
+            }
+
+            // DB connection close
+            pgsqlConn.pgsqlCloseConnection(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
